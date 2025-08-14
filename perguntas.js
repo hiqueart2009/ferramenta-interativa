7328680c3a9a4670e8abfd47c4e4b2c1ba653c98
hiqueart2009 <henrique.artigas@escola.pr.gov.br>
2025-08-14 16:06:48 -0300

    ....

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    videogame,
+    qual o nome do protagonista de call of duty black ops 2?,
+    david mason
+)
+
+criaCartao(
+    videogame,
+    qual o maior rival do xbox?,
+    play station
+)
+
+criaCartao(
+    videogames,
+    em que ano lançou o pac-man?,
+    1980
+)
